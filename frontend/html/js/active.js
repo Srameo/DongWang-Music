f245669ff15d231b2b4da251684222684163b07c
echo <1799956902@qq.com>
2020-07-28 11:54:25 +0800

    new vue

--- frontend/html/js/active.js
+++ frontend/html/js/active.js
@@ -1,14 +1,14 @@
-(function ($) {
+(function (jQuery) {
     'use strict';
-
+    var $=jQuery
     var browserWindow = $(window);
 
-    // :: 1.0 Preloader Active Code
-    browserWindow.on('load', function () {
-        $('.preloader').fadeOut('slow', function () {
-            $(this).remove();
-        });
-    });
+    // // :: 1.0 Preloader Active Code
+    // browserWindow.on('load', function () {
+    //     $('.preloader').fadeOut('slow', function () {
+    //         $(this).remove();
+    //     });
+    // });
 
     // :: 2.0 Nav Active Code
     if ($.fn.classyNav) {
