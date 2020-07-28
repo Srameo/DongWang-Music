b362f0a6d413a7becb60d0235af6351bb1965785
echo <1799956902@qq.com>
2020-07-28 13:54:33 +0800

    add index

--- frontend/music_player/src/App.vue
+++ frontend/music_player/src/App.vue
@@ -11,7 +11,5 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-#app
-	height: 100%
+<style >
 </style>
---
+++ frontend/music_player/src/components/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<!-- 主页 -->
+	<div class="index">
+		<index-top></index-top>
+		<!-- <index-main></index-main> -->
+	</div>
+</template>
+<script>
+import Top from '../index/top.vue'
+
+export default {
+	data () {
+		return {
+		}
+	},
+	methods: {
+	},
+	components: {
+		'index-top': Top,
+		// 'index-main': Main
+	}
+}
+</script>
+<style >
+</style>
