f80693d25270f9d0f5e88fa8dc916c5cbefc6397
lyhbalance <514152738@qq.com>
2020-07-28 23:43:02 +0800

    解决跨域问题

--- backend/user-edge-service/src/main/java/com/music/config/WebConfig.java
+++ backend/user-edge-service/src/main/java/com/music/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "null")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:8082")
                 .allowedMethods("GET","POST","PUT","OPTIONS","DELETE")
                 .allowCredentials(true)
                 .maxAge(3600);
