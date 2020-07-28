5851c08c161161b01947b8c9ff2d7ced961beefb
srameo <870830732@qq.com>
2020-07-28 19:03:17 +0800

    完善接口文档

--- backend/user-edge-service/src/main/java/com/music/util/response/RecommendResponse.java
+++ backend/user-edge-service/src/main/java/com/music/util/response/RecommendResponse.java
@@ -3,17 +3,17 @@ package com.music.util.response;
 import java.util.List;
 
 public class RecommendResponse extends Response {
-    List<Long> recommend_ids;
+    List<Long> ids;
 
-    public RecommendResponse(List<Long> recommend_ids) {
-        this.recommend_ids = recommend_ids;
+    public RecommendResponse(List<Long> ids) {
+        this.ids = ids;
     }
 
-    public List<Long> getRecommend_ids() {
-        return recommend_ids;
+    public List<Long> getIds() {
+        return ids;
     }
 
-    public void setRecommend_ids(List<Long> recommend_ids) {
-        this.recommend_ids = recommend_ids;
+    public void setIds(List<Long> ids) {
+        this.ids = ids;
     }
 }
