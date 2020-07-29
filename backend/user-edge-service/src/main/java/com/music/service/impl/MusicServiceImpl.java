c84624e2ba984bf216d3f69dce32b53d04399f83
srameo <870830732@qq.com>
2020-07-30 00:04:17 +0800

    格式化

--- backend/user-edge-service/src/main/java/com/music/service/impl/MusicServiceImpl.java
+++ backend/user-edge-service/src/main/java/com/music/service/impl/MusicServiceImpl.java
@@ -1,10 +1,10 @@
 package com.music.service.impl;
 
-import com.music.domain.CommentInfo;
-import com.music.domain.HistoryInfo;
-import com.music.domain.StarInfo;
+import com.music.domain.*;
 import com.music.mapper.MusicMapper;
+import com.music.mapper.SearchMapper;
 import com.music.service.MusicService;
+import com.music.service.SearchService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -17,6 +17,9 @@ public class MusicServiceImpl implements MusicService {
     @Resource
     private MusicMapper musicMapper;
 
+    @Resource
+    private SearchMapper searchMapper;
+
     @Override
     public List<String> getAllTags() {
         return musicMapper.getAllTags();
@@ -25,6 +28,17 @@ public class MusicServiceImpl implements MusicService {
     @Override
     public void addNum(int id) {
         musicMapper.addNum(id);
+    }
+
+    @Override
+    public List<SongInfo> getAllStars(int id) {
+        List<SongInfo> songInfos = musicMapper.getAllStars(id);
+        for (SongInfo songInfo : songInfos) {
+            List<SingerInfo> singerInfos = searchMapper.getSingerInfoBySongId(songInfo.getId());
+            songInfo.setSingers(singerInfos);
+//            songInfo.setTags(searchMapper.getStylesByMusicId(songInfo.getId()));
+        }
+        return songInfos;
     }
 
     @Override
