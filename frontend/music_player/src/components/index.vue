5d62f8c9d47c12fd8dface1790b0ac0dac2cb558
srameo <870830732@qq.com>
2020-07-30 16:48:12 +0800

    drawer 样式修改

--- frontend/music_player/src/components/index.vue
+++ frontend/music_player/src/components/index.vue
@@ -6,7 +6,7 @@
       <el-drawer
         :visible.sync="dialogVisable"
         class="dialog"
-        :title="'中软小组007'"
+        
         append-to-body
       >
         <div class="drawer">
@@ -381,7 +381,7 @@ export default {
 }
 
 .drawer {
-  margin: 0 50px 0 50px;
+  margin: 0 10px 0 10px;
 }
 
 .drawer-copywrite {
