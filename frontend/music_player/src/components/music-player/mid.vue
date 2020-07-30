372591a3c5f64334ffc3b2da5e22449796bcbeca
hkx-hkx <1121143810@qq.com>
2020-07-30 11:13:23 +0800

    修改时间戳

--- frontend/music_player/src/components/music-player/mid.vue
+++ frontend/music_player/src/components/music-player/mid.vue
@@ -54,7 +54,7 @@
             <div class="comment-item" v-for="(c, index) in cmts" :key="index">
               <comment :txt="c.content"></comment>
               <div class="comment-time">
-                <span>{{ c.commentTime }}</span>
+                <span>{{c.commentTime | formatDate}}</span>
               </div>
             </div>
           </el-tab-pane>
@@ -255,6 +255,28 @@ export default {
         .catch((err) => {
           console.log(err);
         });
+  },
+  filters: {
+    formatDate: function(value) {
+      // 时间戳转换日期格式方法
+      if (value == null) {
+        return "";
+      } else {
+        let date = new Date(value);
+        let y = date.getFullYear(); // 年
+        let MM = date.getMonth() + 1; // 月
+        MM = MM < 10 ? "0" + MM : MM;
+        let d = date.getDate(); // 日
+        d = d < 10 ? "0" + d : d;
+        let h = date.getHours(); // 时
+        h = h < 10 ? "0" + h : h;
+        let m = date.getMinutes(); // 分
+        m = m < 10 ? "0" + m : m;
+        let s = date.getSeconds(); // 秒
+        s = s < 10 ? "0" + s : s;
+        return y + "-" + MM + "-" + d + " " + h + ":" + m + ":" + s;
+      }
+    },
   },
 };
 </script>
